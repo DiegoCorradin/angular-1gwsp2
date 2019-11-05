823e45a756eb57e15b830a325bf1e2e0b0494dc7
DiegoCorradin <diego.corradin@marconirovereto.it>
2019-11-05 13:35:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-1gwsp2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-1gwsp2)
---
+++ src/app/contatto.component/contatto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Contatto } from './contatto.model';
+
+@Component({
+  selector: 'app-contatto',
+  templateUrl: './contatto.component.html',
+  styleUrls: ['./contatto.component.css']
+})
+
+export class ContattoComponent{
+  contatto: Contatto = new Contatto('Diego', 'Corradin', 'diego.corradin@marconirovereto.it', '123456789', 5)
+}
